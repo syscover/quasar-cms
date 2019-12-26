0723c926693e59cb8fbd2233caa5b239607c9343
Carlos Rodríguez Palacín <carlos.rodriguez@techedgegroup.es>
2019-12-26 12:22:44 +0100

    add attachment family

--- src/database/seeds/CmsResourceSeede.php
+++ src/database/seeds/CmsResourceSeede.php
@@ -8,8 +8,8 @@ class CmsResourceSeeder extends Seeder
     public function run()
     {
         Resource::insert([
-            ['uuid' => '873f3c82-feb8-481a-82c9-f94cc3bda667',      'name' => 'cms',                                'has_field_group' => false,     'package_uuid' => 'dfb01738-2abc-4d42-8c5e-14d75f2b26fe'],
-            ['uuid' => 'bbd5dfe3-e515-40ba-8cd7-4e1da386e366',      'name' => 'cms.family',                         'has_field_group' => true,      'package_uuid' => 'dfb01738-2abc-4d42-8c5e-14d75f2b26fe'],
+            ['uuid' => '873f3c82-feb8-481a-82c9-f94cc3bda667',      'name' => 'cms',                                'has_custom_fields' => false,   'has_attachments' => false,     'package_uuid' => 'dfb01738-2abc-4d42-8c5e-14d75f2b26fe'],
+            ['uuid' => 'bbd5dfe3-e515-40ba-8cd7-4e1da386e366',      'name' => 'cms.family',                         'has_custom_fields' => true,    'has_attachments' => false,     'package_uuid' => 'dfb01738-2abc-4d42-8c5e-14d75f2b26fe'],
         ]);
     }
 }
